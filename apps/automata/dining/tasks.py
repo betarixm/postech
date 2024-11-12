bdd1156b5ff2bc933b9faa168d4150768ce7933f
Minjae Gwon <beta.underalpha@gmail.com>
2024-11-13 00:51:55 +0900

    feat(automata): add task to notify today's meals by type on Discord

--- apps/automata/dining/tasks.py
+++ apps/automata/dining/tasks.py
@@ -1,11 +1,54 @@
 from datetime import date
-from typing import Any
+from typing import Any, Literal
 
 import requests
 from pydining.client.functions import meals_by_period
 from pydining.client.models import Connection as _Connection
 
 from automata.celery import app
+
+
+@app.task
+def notify_today_meals_by_type_on_discord(
+    webhook_url: str,
+    meal_types: list[
+        Literal[
+            "breakfast-a", "breakfast-b", "lunch", "dinner", "staff", "international"
+        ]
+    ],
+):
+    today = date.today()
+
+    meals = meals_by_period(
+        _Connection(protocol="https", host="food.podac.poapper.com", port=443),
+        today,
+        today,
+    )
+
+    message: dict[str, Any] = {
+        "content": "",
+        "tts": False,
+        "embeds": [
+            {
+                "title": today.strftime("%Y. %m. %d. (%A)"),
+                "color": 2326507,
+                "fields": [
+                    {
+                        "name": meal.type,
+                        "value": "\n".join(f"- {food.name}" for food in meal.foods),
+                        "inline": False,
+                    }
+                    for meal in meals
+                    if meal.type in meal_types
+                ],
+            }
+        ],
+        "components": [],
+        "actions": {},
+        "username": "automata",
+    }
+
+    requests.post(webhook_url, json=message)
 
 
 @app.task
